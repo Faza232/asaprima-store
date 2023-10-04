1b76f60ce92af29cae3bd7c5a70be0497c700048
Faza232 <faza2000018060@webmail.uad.ac.id>
2023-10-04 11:04:08 +0700

    repair

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::post('/ulasan', [\App\Http\Controllers\UlasanController::class, 'store'])
 
 //Dashboard
 Route::get('/dashboard', [\App\Http\Controllers\admin\DashboardController::class, 'index'])->name('dashboard.index');
-Route::resource('/dashboard/ulasan', \App\Http\Controllers\admin\DashboardUlasanController::class);
+Route::get('/dashboard/ulasan', [\App\Http\Controllers\admin\DashboardUlasanController::class, 'index'])->name('dashboard.ulasan.index');
