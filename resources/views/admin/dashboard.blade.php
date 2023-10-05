737b19806a99ecc0ba656a6f4c7fc834a5185ff3
sindirella <sindirella151001@gmail.com>
2023-10-05 11:16:28 +0700

    fr

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,6 @@
 @extends('layout.admin')
 @section('container')
   <h1 class="text-3xl font-bold underline">
-    Halaman Dashboard
+
   </h1>
 @endsection
--- resources/views/frontend/ulasan.blade.php
+++ resources/views/frontend/ulasan.blade.php
@@ -1,5 +1,7 @@
 @extends('layout.main')
 @section('container')
+
+<div class="container mx-auto px-40">
 <form action="/ulasan" method="POST">
   @csrf
   <div class="mb-6">
@@ -14,6 +16,9 @@
     <label for="isi" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ulasan Anda</label>
     <input type="text" name="isi" id="isi" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Ulasan Anda" required>
   </div>
-  <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+  <div class="flex justify-center items-center">
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Default</button>
+
 </form>
+</div>
 @endsection
