93dd55c6185e14c779b13acfa831d91c3d5458c1
Faza232 <faza2000018060@webmail.uad.ac.id>
2023-10-16 08:42:51 +0700

    tambahan front end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -202,6 +202,7 @@
     <h3 class="text-gray-600 text-2xl font-medium text-center mt-8 bold">Testimonials</h3>
     <p class="text-lg text-gray-600">What others say about us</p>
 </div>
+@foreach ($ulasan as $ulasan)
 <div class="lg:grid lg:grid-cols-3 lg:gap-x-2">
     <div class="p-4 text-gray-800 rounded-lg shadow-md">
         <div class="mb-2">
@@ -218,36 +219,7 @@
             </div>
         </div>
     </div>
-    <div class="p-4 text-gray-800 rounded-lg shadow-md">
-        <div class="mb-2">
-            <p class="mb-2 text-center text-gray-600 ">
-                " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique sapiente iusto esse. "
-            </p>
-            <div class="flex flex-col items-center justify-center">
-                <div class="w-12 h-12 overflow-hidden bg-gray-100 border-2 border-indigo-100 rounded-full">
-                    <img src="https://cdn.pixabay.com/photo/2021/07/14/17/32/manager-6466713__340.jpg" alt="img"
-                        class="object-cover object-center w-full h-full" />
-                </div>
-                <h5 class="font-bold text-indigo-600">Name</h5>
-                <p class="text-sm text-gray-600">CUstomer</p>
-            </div>
-        </div>
-    </div>
-    <div class="p-4 text-gray-800 rounded-lg shadow-md">
-        <div class="mb-2">
-            <p class="mb-2 text-center text-gray-600 ">
-                " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique sapiente iusto esse. "
-            </p>
-            <div class="flex flex-col items-center justify-center">
-                <div class="w-12 h-12 overflow-hidden bg-gray-100 border-2 border-indigo-100 rounded-full">
-                    <img src="https://cdn.pixabay.com/photo/2021/07/14/17/32/manager-6466713__340.jpg" alt="img"
-                        class="object-cover object-center w-full h-full" />
-                </div>
-                <h5 class="font-bold text-indigo-600">Name</h5>
-                <p class="text-sm text-gray-600">Customer</p>
-            </div>
-        </div>
-    </div>
+@endforeach
 </div>
 
 </div>
