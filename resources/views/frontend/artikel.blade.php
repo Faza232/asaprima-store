e512255e83aa9a22f2625d6f298c0feaaf5ed8d5
Faza232 <faza2000018060@webmail.uad.ac.id>
2023-10-17 11:26:37 +0700

    f

--- resources/views/frontend/artikel.blade.php
+++ resources/views/frontend/artikel.blade.php
@@ -35,11 +35,6 @@
     </a>
     <!-- End Card -->
 
-    <!-- Card -->
-    <a class="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4]" href="#">
-      <div class="aspect-w-16 aspect-h-11">
-        <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1562851529-c370841f6536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80" alt="Image Description">
-      </div>
       <div class="my-6">
         <h3 class="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
           ARTIKEL 2
