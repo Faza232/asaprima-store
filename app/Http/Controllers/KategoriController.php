94cf97f0e0ce5bd2da28c5dc2efb25976af51ba6
Faza232 <faza2000018060@webmail.uad.ac.id>
2023-10-28 11:51:43 +0700

    iuyfdtx

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Kategori;
+
+use Illuminate\Http\Request;
+
+class KategoriController extends Controller
+{
+    public function index(){
+        return view("frontend.kategori");
+    }
+}
---
+++ database/seeders/SertifikatSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SertifikatSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
