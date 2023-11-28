e635c026a45ec38e415c47867f3df9ef91c98502
Faza232 <faza2000018060@webmail.uad.ac.id>
2023-11-28 09:12:00 +0700

    migration fix

--- database/migrations/2023_10_05_020624_create_products_table.php
+++ database/migrations/2023_10_05_020624_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('slug')->unique();
             $table->string('image')->nullable();
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
