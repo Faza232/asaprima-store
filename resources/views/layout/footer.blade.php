2ec8adce2848436c4a85d803aa654ff1f1e93103
sindirella <sindirella151001@gmail.com>
2023-11-29 09:47:41 +0700

    footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,6 +3,12 @@
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
+        <div class="mb-6 md:mb-0">
+              <a href="" class="flex items-center">
+                  <img src="img/APN_Logo.png" class="h-8 me-3" alt="FlowBite Logo" />
+                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">PT ASA PRIMA NIAGA</span>
+              </a>
+          </div>
           <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
                   <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white"></h2>
