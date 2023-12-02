2058df7f60648423605570a39319767157a34c6a
Faza232 <faza2000018060@webmail.uad.ac.id>
2023-12-02 11:17:40 +0700

    gas hosting

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -35,8 +35,8 @@
                             type="password" name="password" id="password" placeholder="Password" />
                         <button
                             type="submit"
-                            class="mt-5 tracking-wide font-semibold bg-main text-white-500 w-full py-4 rounded-lg hover:bg-green-800 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
-                            <span class="ml-">
+                            class="mt-5 tracking-wide font-semibold bg-main text-gray-100 w-full py-4 rounded-lg hover:bg-green-800 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none">
+                            <span class="ml-2">
                                 Sign In
                             </span>
                         </button>
